260d34ea1578872d0dceb24b9ff72f8f0428fdda
ronnypoppelier@gmail.com <ronnypoppelier@gmail.com@4075125f-1a2c-d642-c2ba-d9aff52d0ab7>
2011-09-14 16:10:33 +0000

    Client Gateway voltooid

--- trunk/LoanBroker START/src/client/LoanBrokerGateway.java
+++ trunk/LoanBroker START/src/client/LoanBrokerGateway.java
@@ -1,20 +1,107 @@
 package client;
 
+import client.gui.ClientFrame;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import messaging.JMSSettings;
+
 /**
  *
  * @author Ronny
  */
 public class LoanBrokerGateway {
 
-    public LoanBrokerGateway() {
+    /*
+     * Connection to JMS
+     */
+    private LoanTestClient client;
+    private ClientFrame frame; // GUI
+    private Connection connection; // to connect to the JMS
+    protected Session session; // session for making messages, producers and consumers
+    private ClientSerializer serializer; // for serializing ClientRequest and ClientReply to/from XML
+    private MessageProducer producer; // for sending messages
+    private MessageConsumer consumer; // for receiving messages
+
+    public LoanBrokerGateway(String name, String requestQueue, String replyQueue) throws Exception {
+        // connecting to the JMS
+        Context jndiContext = new InitialContext();
+        ConnectionFactory connectionFactory = (ConnectionFactory) jndiContext.lookup(JMSSettings.CONNECTION);
+        connection = connectionFactory.createConnection();
+        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        // create the serializer
+        serializer = new ClientSerializer();
+
+        // connect to the sender channel
+        Destination senderDestination = (Destination) jndiContext.lookup(requestQueue);
+        producer = session.createProducer(senderDestination);
+        // connect to the receiver channel
+        Destination receiverDestination = (Destination) jndiContext.lookup(replyQueue);
+        consumer = session.createConsumer(receiverDestination);
+        consumer.setMessageListener(new MessageListener() {
+
+            public void onMessage(Message message) {
+                processLoanOffer((TextMessage) message);
+            }
+        });
+
+        // create the GUI
+        frame = new ClientFrame(name) {
+
+            @Override
+            public void send(ClientRequest request) {
+                applyForLoan(request);
+            }
+        };
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+
+                frame.setVisible(true);
+            }
+        });
     }
 
-    private void applyForLoan(ClientRequest request) {
+    public void start() {
+        try {
+            connection.start();
+        } catch (JMSException ex) {
+            ex.printStackTrace();
+        }
     }
 
-    private void start() {
+    public void applyForLoan(ClientRequest request) {
+        try {
+            producer.send(session.createTextMessage(serializer.requestToString(request)));
+            frame.addRequest(request);
+        } catch (JMSException ex) {
+            Logger.getLogger(LoanBrokerGateway.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
-    private void loanOfferArrived(ClientRequest request, ClientReply reply) {
+    public void loanOfferArrived(ClientRequest request, ClientReply reply) {
+        frame.addReply(null, reply);
+    }
+
+    private void processLoanOffer(TextMessage message) {
+        try {
+            ClientReply reply = serializer.replyFromString(((TextMessage) message).getText());
+            loanOfferArrived(null, reply);
+        } catch (JMSException ex) {
+            Logger.getLogger(LoanBrokerGateway.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- trunk/LoanBroker START/src/client/LoanTestClient.java
+++ trunk/LoanBroker START/src/client/LoanTestClient.java
@@ -1,19 +1,7 @@
 package client;
 
-import client.gui.ClientFrame;
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.Destination;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageListener;
-import javax.jms.MessageProducer;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import messaging.JMSSettings;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * This class represents one Clinet Application.
@@ -25,90 +13,30 @@ import messaging.JMSSettings;
  */
 public class LoanTestClient {
 
-    /*
-     * Connection to JMS
-     */
-    private Connection connection; // to connect to the JMS
-    protected Session session; // session for making messages, producers and consumers
-
-    private MessageProducer producer; // for sending messages
-    private MessageConsumer consumer; // for receiving messages
-
-    private ClientSerializer serializer; // for serializing ClientRequest and ClientReply to/from XML
-    
-    private ClientFrame frame; // GUI
+    LoanBrokerGateway gateway;
 
     public LoanTestClient(String name, String requestQueue, String replyQueue) throws Exception {
         super();
-        // connecting to the JMS
-        Context jndiContext = new InitialContext();
-        ConnectionFactory connectionFactory = (ConnectionFactory) jndiContext.lookup(JMSSettings.CONNECTION);
-        connection = connectionFactory.createConnection();
-        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        // connect to the sender channel
-        Destination senderDestination = (Destination) jndiContext.lookup(requestQueue);
-        producer = session.createProducer(senderDestination);
-        // connect to the receiver channel
-        Destination receiverDestination = (Destination) jndiContext.lookup(replyQueue);
-        consumer = session.createConsumer(receiverDestination);
-        consumer.setMessageListener(new MessageListener() {
 
-            public void onMessage(Message message) {
-                processLoanOffer((TextMessage) message);
-            }
-        });
-         // create the serializer
-        serializer = new ClientSerializer();
-         // create the GUI
-        frame = new ClientFrame(name) {
-
-            @Override
-            public void send(ClientRequest request) {
-                sendRequest(request);
-            }
-        };
-
-        java.awt.EventQueue.invokeLater(new Runnable() {
-
-            public void run() {
-
-                frame.setVisible(true);
-            }
-        });
+        gateway = new LoanBrokerGateway(name, requestQueue, replyQueue);
     }
+
     /**
      * Sends new loan request to the LoanBroker.
      * @param request
      */
-    public void sendRequest(ClientRequest request) {
-        try {
-            producer.send(session.createTextMessage(serializer.requestToString(request)));
-            frame.addRequest(request);
-        } catch (JMSException ex) {
-            ex.printStackTrace();
-        }
+    public void sendRequest(int SSN, int amount, int time) {
+        gateway.applyForLoan(new ClientRequest(SSN, amount, time));
     }
-    /**
-     * This message is called whenever a new client reply message arrives.
-     *  The message is de-serialized into a ClientReply, and the reply is shown in the GUI.
-     * @param message
-     */
-    private void processLoanOffer(TextMessage message) {
-        try {
-            ClientReply reply = serializer.replyFromString(((TextMessage) message).getText());
-            frame.addReply(null, reply);
-        } catch (JMSException ex) {
-            ex.printStackTrace();
-        }
-    }
+
     /**
      * Opens connestion to JMS,so that messages can be send and received.
      */
     public void start() {
         try {
-            connection.start();
-        } catch (JMSException ex) {
-            ex.printStackTrace();
+            gateway.start();
+        } catch (Exception ex) {
+            Logger.getLogger(LoanTestClient.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
